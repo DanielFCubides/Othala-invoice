c680e2411bbfc0982c9ef2c4f51143ab47da8309
dafer <fernando.cubides@merlinjobs.com>
2020-07-20 17:14:24 -0500

    feat: add the behavior and the implementation of the product manager

---
+++ app/products/usecases/product_manager.go
@@ -0,0 +1,11 @@
+package usecases
+
+import "othala/app/products"
+
+type ProductManager interface {
+	GetAll() ([]products.Product, error)
+	GetById(productId string) (products.Product, error)
+	Delete(productId string) (products.Product, error)
+	Create(product products.Product) (products.Product, error)
+	Update(product products.Product) (products.Product, error)
+}
