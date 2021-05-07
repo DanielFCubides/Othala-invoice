27644af683a6f1342f2aa9c00f07a8a8964a8c6a
Daniel Fernando Cubides <dfcubidesc@unal.edu.co>
2021-05-06 19:08:19 -0500

    feat: add unit test in adapter layer for create and update products

--- app/products/adapters/rest_test.go
+++ app/products/adapters/rest_test.go
@@ -1,8 +1,11 @@
 package adapters
 
 import (
+	"bytes"
 	"encoding/json"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"othala/app/products"
@@ -32,20 +35,13 @@ func (r RestSuite) TestRest_GetProducts() {
 		Category: "lactose",
 		Type:     "freezer",
 	}}, nil)
-	res, err := http.NewRequest(http.MethodGet, "/products/", nil)
 
-	if err != nil {
-		r.Fail("could not create request")
+	recoder, err := r.callAdapter(http.MethodGet, "/products/", nil, r.adapter.GetProducts)
+	if recoder == nil {
+		r.Fail("could not call adapter")
+		return
 	}
-
-	recoder := httptest.NewRecorder()
-	handler := http.HandlerFunc(r.adapter.GetProducts)
-	handler.ServeHTTP(recoder, res)
-
-	if status := recoder.Code; status != http.StatusOK {
-		r.Fail("error not ok")
-	}
-
+	r.Equal(http.StatusOK, recoder.Code)
 	var response []*ProductResponse
 	err = json.NewDecoder(recoder.Body).Decode(&response)
 	if err != nil {
@@ -53,5 +49,91 @@ func (r RestSuite) TestRest_GetProducts() {
 		return
 	}
 	r.Equal(productName, response[0].Name)
+}
 
+func (r RestSuite) TestRest_GetProductByID() {
+	productName := "Milk"
+	r.useCase.Mock.On("GetById", mock.Anything).
+		Return(products.Product{
+			Name:     productName,
+			Category: "lactose",
+			Type:     "freezer",
+		}, nil)
+
+	recoder, err := r.callAdapter(http.MethodGet, "/products/1/", nil, r.adapter.GetProductById)
+	if recoder == nil {
+		r.Fail("could not call adapter")
+		return
+	}
+	r.Equal(http.StatusOK, recoder.Code)
+	var response *ProductResponse
+	err = json.NewDecoder(recoder.Body).Decode(&response)
+	if err != nil {
+		r.Fail("could not parse response")
+		return
+	}
+	r.Equal(productName, response.Name)
+}
+
+func (r RestSuite) TestRest_CreateProduct() {
+	productName := "Milk"
+	product := products.Product{
+		Name:     productName,
+		Category: "lactose",
+		Type:     "freezer",
+	}
+	r.useCase.Mock.On("Create", mock.Anything).
+		Return(product, nil)
+	b, err := json.Marshal(product)
+	reader := bytes.NewBuffer(b)
+	recoder, err := r.callAdapter(http.MethodPost, "/products/", reader, r.adapter.CreateProduct)
+	if recoder == nil {
+		r.Fail("could not call adapter")
+		return
+	}
+	r.Equal(http.StatusOK, recoder.Code)
+	var response *ProductResponse
+	err = json.NewDecoder(recoder.Body).Decode(&response)
+	if err != nil {
+		r.Fail("could not parse response")
+		return
+	}
+	r.Equal(productName, response.Name)
+}
+
+func (r RestSuite) TestRest_UpdateProduct() {
+	productName := "Milk"
+	product := products.Product{
+		Name:     productName,
+		Category: "lactose",
+		Type:     "freezer",
+	}
+	r.useCase.Mock.On("Update", mock.Anything).
+		Return(product, nil)
+	b, err := json.Marshal(product)
+	reader := bytes.NewBuffer(b)
+	recoder, err := r.callAdapter(http.MethodPost, "/products/1/", reader, r.adapter.UpdateProduct)
+	if recoder == nil {
+		r.Fail("could not call adapter")
+		return
+	}
+	r.Equal(http.StatusOK, recoder.Code)
+	var response *ProductResponse
+	err = json.NewDecoder(recoder.Body).Decode(&response)
+	if err != nil {
+		r.Fail("could not parse response")
+		return
+	}
+	r.Equal(productName, response.Name)
+}
+
+func (r RestSuite) callAdapter(method, url string, body io.Reader, adapter http.HandlerFunc) (*httptest.ResponseRecorder, error) {
+	res, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	recoder := httptest.NewRecorder()
+	handler := adapter
+	handler.ServeHTTP(recoder, res)
+	return recoder, err
 }
