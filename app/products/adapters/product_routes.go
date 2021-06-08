a1b274648c30621bc2b923f6bbe7b22a0814c560
dfcubidesc <dfcubidesc@unal.edu.co>
2021-06-08 11:52:48 -0500

    refactor: update rest adapter and url routes

--- app/products/adapters/product_routes.go
+++ app/products/adapters/product_routes.go
@@ -3,9 +3,9 @@ package adapters
 import "github.com/go-chi/chi"
 
 func RegisterProductRoutes(r chi.Router, adapter *RestAdapter) {
-	r.Get("/products", adapter.GetProducts)
-	r.Get("/products/{productId}", adapter.GetProductById)
-	r.Post("/products", adapter.CreateProduct)
-	r.Put("/products", adapter.UpdateProduct)
-	r.Delete("/products/{productId}", adapter.DeleteProduct)
+	r.Get("/products/", adapter.GetProducts)
+	r.Get("/products/{productId}/", adapter.GetProductById)
+	r.Post("/products/", adapter.CreateProduct)
+	r.Put("/products/{productId}/", adapter.UpdateProduct)
+	r.Delete("/products/{productId}/", adapter.DeleteProduct)
 }
--- app/products/adapters/rest.go
+++ app/products/adapters/rest.go
@@ -26,24 +26,34 @@ func newRestAdapter(manager usecases.ProductManager) *RestAdapter {
 }
 
 func (adapter *RestAdapter) GetProducts(writer http.ResponseWriter, r *http.Request) {
-	response, _ := adapter.manager.GetAll()
+	response, err := adapter.manager.GetAll()
+	if err != nil {
+		writer.WriteHeader(http.StatusUnprocessableEntity)
+		return
+	}
 	writer.Header().Set("Content-Type", "application/json")
-	writer.WriteHeader(200)
+	writer.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(writer).Encode(response)
 }
 
 func (adapter *RestAdapter) UpdateProduct(writer http.ResponseWriter, request *http.Request) {
-	product := products.Product{}
+	var productRequest ProductRequest
+	err := json.NewDecoder(request.Body).Decode(&productRequest)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	product := productRequest.mapToProduct()
 	response, _ := adapter.manager.Update(product)
 	writer.Header().Set("Content-Type", "application/json")
-	writer.WriteHeader(200)
+	writer.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(writer).Encode(response)
 }
 
 func (adapter *RestAdapter) GetProductById(writer http.ResponseWriter, request *http.Request) {
 	response, _ := adapter.manager.GetById("product")
 	writer.Header().Set("Content-Type", "application/json")
-	writer.WriteHeader(200)
+	writer.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(writer).Encode(response)
 }
 
@@ -51,13 +61,13 @@ func (adapter *RestAdapter) CreateProduct(writer http.ResponseWriter, request *h
 	product := products.Product{}
 	response, _ := adapter.manager.Create(product)
 	writer.Header().Set("Content-Type", "application/json")
-	writer.WriteHeader(200)
+	writer.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(writer).Encode(response)
 }
 
 func (adapter *RestAdapter) DeleteProduct(writer http.ResponseWriter, request *http.Request) {
 	response, _ := adapter.manager.Delete("product")
 	writer.Header().Set("Content-Type", "application/json")
-	writer.WriteHeader(200)
+	writer.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(writer).Encode(response)
 }
