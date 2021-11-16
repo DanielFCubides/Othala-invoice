db85cefefa9d3855e25ea11f4a5002f93038a76f
dfcubidesc <dfcubidesc@unal.edu.co>
2021-11-16 07:21:06 -0500

    feat: add cros origin middleware

--- app/deliveries/rest.go
+++ app/deliveries/rest.go
@@ -2,6 +2,7 @@ package deliveries
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"log"
 	"net/http"
 	"os"
@@ -13,6 +14,16 @@ func Run() {
 	err := config.Injector.Invoke(
 		func(productAdapter *adapters.RestAdapter) error {
 			router := chi.NewRouter()
+			router.Use(cors.Handler(cors.Options{
+				// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+				AllowedOrigins:   []string{"https://*", "http://*"},
+				// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+				AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+				AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+				//ExposedHeaders:   []string{"Link"},
+				//AllowCredentials: false,
+				//MaxAge:           300, // Maximum value not ignored by any of major browsers
+			}))
 			port := os.Getenv("PORT")
 			registerCommonRoutes(router)
 			router.Route("/v1", func(apiRouter chi.Router) {
